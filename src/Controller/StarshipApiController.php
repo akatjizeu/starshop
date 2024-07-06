c5c73a15cd188dd308bdbb55c7702321f3d67e21
Vakaa <akatjizeu@gmail.com>
2024-07-06 07:14:08 +0200

    API and serializer

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Model\Starship;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,24 +13,9 @@ class StarshipApiController extends AbstractController
     public function getCollection():Response
     {
         $starships =[
-            [
-                'name' => 'Uaueza',
-                'class' => '4A',
-                'captain' => 'Ueii',
-                'status' => 'Beautiful',
-            ],
-            [
-                'name' => 'Casa',
-                'class' => '4B',
-                'captain' => 'CASA',
-                'status' => 'Better',
-            ],
-            [
-                'name' => 'Dasa',
-                'class' => '4C',
-                'captain' => 'DASA',
-                'status' => 'Not At all',
-            ]
+            new Starship(1, 'Uaueza', '4A','Ueii','Beautiful'),
+            new Starship(2,'Casa','4B','CASA','Better'),
+            new Starship(3,'Dasa','4C','DASA','BNot At all')
         ] ;
         return $this->json($starships);
     }
--- src/Model/Starship.php
+++ src/Model/Starship.php
@@ -10,6 +10,36 @@ class Starship
         private string $class,
         private string $captain,
         private string $status,
-    ){}
+    )
+    {
+
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getClass(): string
+    {
+        return $this->class;
+    }
+
+    public function getCaptain(): string
+    {
+        return $this->captain;
+    }
+
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+
 
 }
