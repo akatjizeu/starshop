bece93c5bf85a0689022a82c6b820461f8b11eca
Vakaa <akatjizeu@gmail.com>
2024-07-06 08:13:48 +0200

    Dynamic routes

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -9,14 +9,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/api/starships')]
 class StarshipApiController extends AbstractController
 {
-    #[Route('api/starships', name: 'starships')]
+    #[Route( name: 'starships', methods: ['GET'])]
     public function getCollection( StarshipRepository $repository):Response
     {
 
         $starships = $repository->findAll();
         return $this->json($starships);
+    }
+
+
+    #[Route('/{id<\d+>}', name: 'starshipsNum',methods: ['GET'])]
+    public function get(int $id, StarshipRepository $repository):Response
+    {
+        $starship = $repository->find($id);
+
+        if(!$starship)
+        {
+            throw $this->createNotFoundException('The starship does not exist');
+        }
+        return $this->json($starship);
     }
 
 }
