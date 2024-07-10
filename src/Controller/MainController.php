c8f46e71e8f611816d333b864ee3d061d16bcbe0
akatjizeu <akatjizeu@gmail.com>
2024-07-10 17:37:37 +0200

    Components cache

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -7,19 +7,39 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class MainController extends AbstractController
 {
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     */
     #[Route('/', name: 'app_homepage')]
-    public function homepage(StarshipRepository $starshipRepository, HttpClientInterface $client): Response
+    public function homepage(
+        StarshipRepository $starshipRepository,
+        HttpClientInterface $client,
+        CacheInterface $cache,
+    ): Response
     {
         $ships = $starshipRepository->findAll();
         $myShip = $ships[array_rand($ships)];
 
-        $response = $client ->request('GET', 'https://api.wheretheiss.at/v1/satellites/25544');
-
-        $issData = $response ->toArray();
+        $issData = $cache->get('iss_location_data',function(ItemInterface $item) use($client):array{
+            $item->expiresAfter(5);
+            $response = $client ->request('GET', 'https://api.wheretheiss.at/v1/satellites/25544');
+            return $issData = $response ->toArray();
+        });
 
 
 
